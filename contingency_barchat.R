adbc7f0e4984f15383eee4d8562556d1025ab2c7
Chirusampathh <cv24aao@herts.ac.uk>
2025-12-04 17:11:40 +0000

    Fix contingency table script and correct bar chart code

--- contingency_barchat.R
+++ contingency_barchat.R
@@ -1,23 +1,24 @@
-data <- read.csv("tests.csv")   
-camera_data <- data[, c("fc" "pc")]   
+data <- read.csv("test.csv")
+
+camera_data <- data[, c("fc", "pc")]
+
 camera_data$pc_group <- cut(
-  camera_data$pcx,   
-  breaks = c(-Inf, 5, 15, Inf)
+  camera_data$pc,
+  breaks = c(-Inf, 5, 15, Inf),
   labels = c("Low (≤5 MP)", "Medium (6–15 MP)", "High (≥16 MP)")
-)   
+)
 
 camera_data$fc_group <- cut(
   camera_data$fc,
   breaks = c(-Inf, 3, 10, Inf),
-  label = c("Low (≤3 MP)", "Medium (4–10 MP)", "High (≥11 MP)")
-)  
+  labels = c("Low (≤3 MP)", "Medium (4–10 MP)", "High (≥11 MP)")
+)
 
 camera_table <- table(camera_data$pc_group, camera_data$fc_group)
 camera_table   
 
-
 barplot(
-  camera_tab,  
+  camera_table,
   beside = TRUE,
   main = "Primary vs Front Camera Resolution Categories",
   xlab = "Front camera group",
