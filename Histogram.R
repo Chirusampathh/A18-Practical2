57a6c14545f0419c4ffc947d9d799f3b841a0a4a
Chirusampathh <cv24aao@herts.ac.uk>
2025-12-10 16:00:38 +0000

    Updated Histogram.R
    Adjusted Clear titles, axes with units and proper scales.

--- Histogram.R
+++ Histogram.R
@@ -1,12 +1,29 @@
-par(mfrow = c(1, 2))  
+par(mfrow = c(1, 2))
 
-hist(camera_data$fc,
+fc <- camera_data$fc
+pc <- camera_data$pc
+
+hist(fc,
      main = "Histogram of Front Camera Resolution",
      xlab = "Front camera resolution (megapixels)",
-     ylab = "Frequency")
-
-hist(camera_data$pc,
+     ylab = "Density",
+     freq = FALSE)
+curve(dnorm(x,
+            mean = mean(fc, na.rm = TRUE),
+            sd   = sd(fc, na.rm = TRUE)),
+      add = TRUE,
+      lwd = 2,
+      col = "red")
+hist(pc,
      main = "Histogram of Primary Camera Resolution",
      xlab = "Primary camera resolution (megapixels)",
-     ylab = "Frequency")
+     ylab = "Density",
+     freq = FALSE)
+curve(dnorm(x,
+            mean = mean(pc, na.rm = TRUE),
+            sd   = sd(pc, na.rm = TRUE)),
+      add = TRUE,
+      lwd = 2,
+      col = "red")
+
 par(mfrow = c(1, 1))
